a65d705b4d8d4a30fd1d0be0cc1f1d01ad752af4
chanmin-jeon <chanminjeon@outlook.com>
2024-11-26 12:32:47 -0700

    deleted some unecessary files

--- app/src/main/java/com/example/syntaxeventlottery/OrganizerController.java
+++
@@ -1,2 +0,0 @@
-package com.example.syntaxeventlottery;public class OrganizerController {
-}
--- app/src/main/java/com/example/syntaxeventlottery/OrganizerControllerListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.syntaxeventlottery;
-
-import java.util.List;
-
-public interface OrganizerControllerListener{
-    public void onEventsLoaded(List<Event> eventList);
-    public void onEventSaved();
-    public void onError(String errorMessage);
-}
